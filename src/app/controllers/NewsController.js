3cc1ec87161940f4d230387668751a281b988239
chungnguyenbao <babaolan1982@gmail.com>
2023-05-03 09:52:14 +0700

    Refactor code: create routers, app/controllers folders

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,11 @@
+class NewsController{
+    //[Get] / news
+    index(req,res){
+        res.render('news');
+    }
+    // [get] /news/:slug
+    show(req, res){
+        res.send('New detail')
+    }
+}
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
 const path = require('path');
+const router = require('./routers');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(morgan('combined'));
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
+
+// app.use(morgan('combined'));
 app.engine('hbs', handlebars.engine({
   extname : '.hbs'
 }));
@@ -16,15 +22,9 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ',path.join(__dirname, 'resources/views') );
 
-app.get('/', (req, res) => {
-  // return res.send('Hello World!')
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  // return res.send('Hello World!')
-  res.render('news');
-});
+//-------------------Routing--------- ------
 
+router(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
